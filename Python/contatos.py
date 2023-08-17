f2c59869d0554972fbeed264e2015fc50372a1f3
Maia20-21 <maiabruna70@gmail.com>
2023-08-16 23:02:22 -0300

    Update contatos.py

--- Python/contatos.py
+++ Python/contatos.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, request, flash, redirect
-import json
+from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def contatos():
+    return "Hello world"
+
+if __name__== "__main__":
+    app.run()
+
